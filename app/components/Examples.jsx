ce88abf14be68647f47646516d73c8b05b456711
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-10-20 12:15:59 +0530

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,11 @@ var React = require('react');
 var Examples = React.createClass({
     render: function() {
         return(
+            <div>
             <h1>Examples Component</h1>
+            <h2>welcome</h2>
+            </div>
+
         );
     }
 });
