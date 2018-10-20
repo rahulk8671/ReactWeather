b8bfa3029bd57d154a5e1e39ca3c86f79a5e7db1
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-10-20 12:43:02 +0530

    prep heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-    if(req.header['x-forwarded=proto'] === 'http') {
+    if(req.header['x-forwarded-proto'] === 'http') {
         next();
     }else {
         res.redirect('https://'+req.hostname+req.url);
