c86a71cb5c25217d52110c3436473b8cd738ba83
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-10-22 14:15:32 +0530

    update middle

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
+// app.use(function (req, res, next){
+//     if(req.headers['x-forwarded-proto'] === 'http') {
+//         next();
+//     }else {
+//         res.redirect('http://' + req.hostname + req.url);
+//     }
+// });
+
 app.use(function (req, res, next){
-    if(req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    }else {
+    if(req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
+    }else {
+        next();
     }
 });
 
